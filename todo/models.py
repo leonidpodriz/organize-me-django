0f5ecc3a0a6d9c2eb0487afe38ee0a684a04375f
Leonid Podriz <leonidpodriz@gmail.com>
2021-04-07 15:13:38 +0000

    create launch configuation

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from uuid import uuid4
 
-# Create your models here.
+
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    subject = models.CharField(max_length=300)
+    description = models.TextField(blank=True, default="")
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
