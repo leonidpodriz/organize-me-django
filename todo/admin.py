2423bbcbf025af2b98c371cbe5fa3f7ccdc178b3
Leonid Podriz <leonidpodriz@gmail.com>
2021-04-07 17:10:40 +0000

    create tag, comment and attachment models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo, Tag, TodoComment
+
+
+class TodoCommentInline(admin.StackedInline):
+    extra = 1
+    model = TodoComment
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    inlines = (TodoCommentInline,)
--- todo/models.py
+++ todo/models.py
@@ -3,8 +3,35 @@ from django.contrib.auth import get_user_model
 from uuid import uuid4
 
 
+User = get_user_model()
+
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=20)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Todo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     subject = models.CharField(max_length=300)
     description = models.TextField(blank=True, default="")
-    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+
+
+class TodoComment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    todo = models.ForeignKey(
+        Todo, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField(blank=True, default="")
+
+
+class TodoCommentAttachment(models.Model):
+    file = models.FileField(upload_to='%Y/%m/%d')
+    comment = models.ForeignKey(
+        TodoComment, on_delete=models.CASCADE, related_name="files")
